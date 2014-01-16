ea3a93ad047c24493b36c0f82997d09641c14bad
ma34s <masashi.seino@gmail.com>
2014-01-16 23:13:08 +0900

    fix typo

--- make_japanized_ROM_SC03E.bat
+++ make_japanized_ROM_SC03E.bat
@@ -7,7 +7,7 @@ set /p mode="One-segì¸ÇÍÇ‹Ç∑Ç©ÅH(Y/N) "
 
 SET job=sc03e
 
-if %mode%%job%==N%job% SET job=sc02e_no_one-seg
+if %mode%%job%==N%job% SET job=sc03e_no_one-seg
 
 ::cscript make_japanized_ROM.wsf %1 //nologo //Job:%job% /dbg:ON /dbg_gui:ON
 cscript make_japanized_ROM.wsf %1 //nologo //Job:%job%
